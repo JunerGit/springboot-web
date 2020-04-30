d5575e2bc8177dd314f785ea0dbd4d61e33c2ee6
JunerGit <64068465+JunerGit@users.noreply.github.com>
2020-04-30 11:27:50 +0800

    Update Application.java

--- src/main/java/vip/juner/springboot/web/system/application/Application.java
+++ src/main/java/vip/juner/springboot/web/system/application/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * <p></p>
  * <p></p>
  * <p> SpringBootApplication 类属性 </p>
- * <pre>  { exclude, excludeName, scanBasePackages, scanBasePackageClasses, proxyBeanMethods} </pre>
+ * <pre>  { exclude, excludeName, scanBasePackages, scanBasePackageClasses, proxyBeanMethods } </pre>
  * <p> <b>scanBasePackages：</b>指定 Spring Boot 扫描路径，默认为 Spring Boot当前路径及子路径</p>
  * <p> <b>SpringBoot模版引擎：</b> </p>
  * <pre> { Thymeleaf, FreeMarker, Velocity, Groovy, Mustache } </pre>
